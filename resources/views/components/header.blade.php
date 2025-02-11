7cfaebc9bbeb3ef552bb681ecf76902788f48002
fajarajiprayoga <fajaraji.prayoga@gmail.com>
2025-02-11 12:03:40 +0700

    Edit: responsive header menu on mobile view

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,35 +1,35 @@
 <div style="background-image: url('{{ asset('assets/background/bg-header.jpg') }}');" class="bg-cover bg-center">
-    <div class="h-14 overflow-x-auto flex justify-center lg:gap-6 gap-3 items-center" style="background: linear-gradient(to bottom, rgba(236, 229, 200, 1), rgba(236, 229, 200, 0) 180%);">
+    <div class="h-14 overflow-x-auto flex justify-center lg:gap-6 gap-2 lg:gap-3 items-center" style="background: linear-gradient(to bottom, rgba(236, 229, 200, 1), rgba(236, 229, 200, 0) 180%);">
         <div class="flex items-center">
             <span class="h-3 w-3 rounded-full" style="background-color: #DF3727"></span>
         </div>
         <div class="flex items-center">
             <a href="{{route('home')}}">
-                <span class="text-lg font-medium" style="color: #DF3727">Beranda</span>
+                <span class="text-base lg:text-lg font-semibold lg:font-medium" style="color: #DF3727">Beranda</span>
             </a>
         </div>
         <div class="flex items-center">
-            <span class="h-3 w-3 rounded-full" style="background-color: #DF3727"></span>
+            <span class="h-3 w-3 rounded-full hidden lg:block" style="background-color: #DF3727"></span>
         </div>
         <div class="flex items-center">
             <a href="{{route('home')}}#event-tab">
-                <span class="text-lg font-medium" style="color: #DF3727">Acara</span>
+                <span class="text-base lg:text-lg font-semibold lg:font-medium" style="color: #DF3727">Acara</span>
             </a>
         </div>
         <div class="flex items-center">
-            <span class="h-3 w-3 rounded-full" style="background-color: #DF3727"></span>
+            <span class="h-3 w-3 rounded-full hidden lg:block" style="background-color: #DF3727"></span>
         </div>
         <div class="flex items-center">
             <a href="{{route('information')}}">
-                <span class="text-lg font-medium" style="color: #DF3727">Informasi</span>
+                <span class="text-base lg:text-lg font-semibold lg:font-medium" style="color: #DF3727">Informasi</span>
             </a>
         </div>
         <div class="flex items-center">
-            <span class="h-3 w-3 rounded-full" style="background-color: #DF3727"></span>
+            <span class="h-3 w-3 rounded-full hidden lg:block" style="background-color: #DF3727"></span>
         </div>
         <div class="flex items-center">
             <a href="{{$setting->gdrive_link}}" target="_blank">
-                <span class="text-lg font-medium" style="color: #DF3727">Galeri</span>
+                <span class="text-base lg:text-lg font-semibold lg:font-medium" style="color: #DF3727">Galeri</span>
             </a>
         </div>
         <div class="flex items-center">
